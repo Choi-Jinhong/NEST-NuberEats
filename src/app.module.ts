b0cae937c432d00e5b3be971bd488b94dc1dc09a
Gnong <jinhongdev@gmail.com>
2021-04-12 15:04:15 +0900

    Introducing ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:process.env.NODE_ENV === "dev" ? ".dev.env" : ".test.env",
+    }),
+    TypeOrmModule.forRoot({
+      "type": "postgres",
+      "host": "localhost",
+      "port": 5432,
+      "username": "gnong",
+      "password": "12345",
+      "database": "nuber-eats",
+      "synchronize": true,
+      "logging": true,
+    }),
     GraphQLModule.forRoot({
-      autoSchemaFile:true,
+      autoSchemaFile: true,
     }),
     RestaurantsModule,
   ],
