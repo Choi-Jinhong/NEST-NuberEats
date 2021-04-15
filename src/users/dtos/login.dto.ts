40fc82eaf1a232d487da579202158c3d0135ff41
Gnong <jinhongdev@gmail.com>
2021-04-15 19:44:12 +0900

    Login part Two

--- src/users/dtos/login.dto.ts
+++ src/users/dtos/login.dto.ts
@@ -3,10 +3,10 @@ import { MutationOutput } from 'src/common/dtos/output.dto';
 import { User } from '../entities/user.entities';
 
 @InputType()
-export class LoginInput extends PickType(User, ["email", "password"]) {}
+export class LoginInput extends PickType(User, ['email', 'password']) {}
 
 @ObjectType()
 export class LoginOutput extends MutationOutput {
-  @Field((type) => String)
-  token: string;
+  @Field((type) => String, { nullable: true })
+  token?: string;
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateAccountInput } from './dtos/create-account-dto';
+import { LoginInput } from './dtos/login.dto';
 import { User } from './entities/user.entities';
 
 @Injectable()
@@ -10,12 +11,16 @@ export class UsersService {
     @InjectRepository(User) private readonly users: Repository<User>,
   ) {}
 
-  async createAccount({ email, password, role }: CreateAccountInput): Promise<[boolean, string?]> {
+  async createAccount({
+    email,
+    password,
+    role,
+  }: CreateAccountInput): Promise<[boolean, string?]> {
     // check new user
     try {
       const exists = await this.users.findOne({ email });
       if (exists) {
-        return [false, "There is a user with that email already"];
+        return [false, 'There is a user with that email already'];
       }
       await this.users.save(this.users.create({ email, password, role }));
       return [true];
@@ -23,5 +28,40 @@ export class UsersService {
       return [false, "Couldn't create account"];
     }
     // create user & hash the password
+  }
+
+  async login({
+    email,
+    password,
+  }: LoginInput): Promise<{ ok: boolean; error?: string; token?: string }> {
+    // find the user with the email
+    try {
+      const user = await this.users.findOne({ email });
+      if (!user) {
+        return {
+          ok: false,
+          error: 'User not found',
+        };
+      }
+    // check if the password is correct
+      const passwordCorrect = await user.checkPassword(password);
+      if(!passwordCorrect){
+        return {
+          ok: false,
+          error: "Wrong password"
+        }
+      }
+      return {
+        ok: true,
+        token: "lalalaalllla"
+      }
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      };
+    }
+
+    // make JWT and give it to the user
   }
 }
